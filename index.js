a6603403fc26ab7eeca68f9f017786f0e63c51e9
Makhaola Kuena <mpkuena@gmail.com>
2024-07-19 14:22:13 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/service-platform', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
